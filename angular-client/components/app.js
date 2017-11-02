9fe833955386eb1d7030272f99ea717b14ba37d1
ccm36 <cameron.c.malloy@gmail.com>
2017-11-01 21:47:44 -0500

    End progress on day1, need to implement search function

--- angular-client/components/app.js
+++ angular-client/components/app.js
@@ -1,12 +1,18 @@
 angular.module('app')
 .controller('AppCtrl', function(itemsService) {
+  
   itemsService.getAll((data) => {
-    console.log(data);
     this.beers = data;
     this.currentBeer = this.beers[0];
   }),
+
   this.selectBeer = (beer) => {
     this.currentBeer = beer;
+  },
+
+  this.updateBeers = (beers) => {
+    this.beers = beers;
+    this.currentBeer = this.beers[0];
   }
 })
 .component('app', {
--- angular-client/components/search.js
+++ angular-client/components/search.js
@@ -1,19 +1,14 @@
 angular.module('app')
 
 .controller('SearchController', function() {
-  this.handleClick = () => {
-    this.service.search(this.input, (data) => {
-      console.log(data);
-      this.result(data);
-    });
-  };
+  
 })
+
 .component('search', {
 
   bindings: {
-    service: '<',
-    result: '<'
+    onUpdate: '<',
   },
   controller: 'SearchController',
   templateUrl: '/templates/search.html'
-  });
+});
